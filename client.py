e635ff5485857be35c6b666cede1a132776d4efe
natalia diaz ayuste <ndiaz@iota22.aulas.gsyc.es>
2014-10-20 17:04:40 +0200

    ej 7, introduce valor de expires

--- client.py
+++ client.py
@@ -9,6 +9,10 @@ import sys
 
 # Cliente UDP simple.
 
+if len(sys.argv) != 6:
+    print "Usage: client.py ip puerto register sip_address expires_value"
+    raise SystemExit
+
 # Dirección IP del servidor.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
@@ -16,8 +20,10 @@ PORT = int(sys.argv[2])
 # Contenido que vamos a enviar
 METODO = sys.argv[3].upper()
 DIREC = sys.argv[4]
+EXPIRES = sys.argv[5]
 
-LINE = METODO + " sip:" + DIREC + " SIP/1.0" + "\r\n"
+LINE = METODO + " sip:" + DIREC + " SIP/2.0" + "\r\n"
+LINE = LINE + "Expires: " + EXPIRES + "\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
