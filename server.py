3293860679a217b1b959af74ed3076bc026713eb
natalia diaz ayuste <ndiaz@iota37.aulas.gsyc.es>
2014-10-20 10:47:17 +0200

    ej 5, añado argumento e imprime puerto cliente

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ en UDP simple
 """
 
 import SocketServer
+import sys
+
+PORT = int(sys.argv[1])
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -15,6 +18,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
+        print self.client_address
         self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -25,6 +29,6 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
+    serv = SocketServer.UDPServer(("", PORT), EchoHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
