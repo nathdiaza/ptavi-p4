14b9f4b4b3d98486ffbd07f20bf490de2328744f
Roger Urrutia Bayo <roger@epsilon30.aulas.gsyc.es>
2014-10-21 18:01:37 +0200

    apartado 7 de la practica

--- server.py
+++ server.py
@@ -15,25 +15,35 @@ except ValueError:
     print "Error: los parametros pasados son erroneos"
     sys.exit()
 
-class EchoHandler(SocketServer.DatagramRequestHandler):
+d = {}
+
+class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             inf_client = self.client_address
-            print "IP: " + str(inf_client[0]) + " Puerto: " + str(inf_client[1])
-            print "El cliente nos manda " + line
-            if not line:
+            ip = str(inf_client[0])
+            puerto = str(inf_client[1])
+            if line:
+                if line [0:8] == "REGISTER":
+                    self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+                    d[ip] = puerto
+                    print d
+                else:
+                    self.wfile.write("Hemos recibido tu peticion")
+                    print "IP: " + ip + " Port: " + puerto
+                    print "El cliente nos manda " + line + "\n"
+            else: # elif not line:
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", PORT), EchoHandler)
+    serv = SocketServer.UDPServer(("", PORT), SIPRegisterHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
