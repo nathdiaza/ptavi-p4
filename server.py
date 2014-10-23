bed6521852c16e90f236bda6fa4296ca484a7396
javier martinez molina <molina@iota09.aulas.gsyc.es>
2014-10-23 18:25:04 +0200

    apartado 8: Intento borrar la linea del usuario Expired del fichero (sin exito)

--- server.py
+++ server.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-"""
-Clase (y programa principal) para un servidor de eco
-en UDP simple
-"""
 
 import SocketServer
 import sys
@@ -13,16 +9,24 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
 
     dic_registro = {}
     fich = open("registered.txt", "a")
-    fich.write("User\t" + "IP\t" + "Expires\n")
+    fich.write("User\t\t\t\t" + "IP\t\t\t" + "Expires\n")
     fich.close()
     
-    def register2file(self, usuario, ip, tiempo):
-
-        fich = open("registered.txt", "a")
-        fich.write(usuario +"\t" + ip + "\t" + tiempo +"\n")
-        fich.close()
-
-
+    def register2file(self, usuario, ip, tiempo, boolean):
+        if boolean:
+            fich = open("registered.txt", "a")
+            fich.write(usuario +"\t" + ip + "\t" + tiempo +"\n")
+            fich.close()
+        else:
+            fich = open("registered.txt", "r+")
+            lines = fich.readlines()
+            for line in lines:
+                palabras_line = line.split()
+                print palabras_line
+                if palabras_line[0] == usuario:
+                    lines.remove(line)
+            fich.close()
+            
     def handle(self):
     
         while 1:
@@ -30,19 +34,20 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             line = self.rfile.read()
             if line != "":
                 list_palabras = line.split()
-            if list_palabras[0] == "REGISTER":
-                recorte = list_palabras[1].split(":")
-                mail = recorte[1]
-                self.wfile.write("SIP/2.0 200 OK\r\n")
-                tiempo = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
-                self.register2file(mail, self.client_address[0], tiempo)
-                self.dic_registro[mail] = self.client_address[0]
-                print self.dic_registro
-                if int(list_palabras[4]) == 0:
-                    del self.dic_registro[mail]
+                if list_palabras[0] == "REGISTER":
+                    recorte = list_palabras[1].split(":")
+                    mail = recorte[1]
+                    self.wfile.write("SIP/2.0 200 OK\r\n")
+                    tiempo = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+                    self.register2file(mail, self.client_address[0], tiempo, 1)
+                    self.dic_registro[mail] = self.client_address[0]
                     print self.dic_registro
-            print self.client_address
-            print line
+                    if int(list_palabras[4]) == 0:
+                        self.register2file(mail, self.client_address[0], tiempo, 0)
+                        del self.dic_registro[mail]
+                        print self.dic_registro
+                print self.client_address
+                print line
             if not line:
                 break
 
