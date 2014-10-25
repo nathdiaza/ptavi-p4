0d89d99433d78ac2f015afff16cb3a01343a0eac
Irene <irene@ubuntu.(none)>
2014-10-25 19:17:57 +0200

    Ejercicio 7: Añadido expires_value

--- client.py
+++ client.py
@@ -7,12 +7,18 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
+
+if len(sys.argv) != 6:
+    print "Usage: client.py ip puerto register sip_address expires_value"
+    raise SystemExit
+    
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 USUARIO = sys.argv[4]
+EXPIRES = sys.argv[5]
 
 # Contenido que vamos a enviar
 #LINE = ' '.join(sys.argv[3:])
@@ -23,7 +29,11 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 #Creamos la petición de registro
-LINE = "REGISTER" + " sip:" + USUARIO + " SIP/2.0" + '\r\n\r\n'
+LINE = "REGISTER" + " sip:" + USUARIO + " SIP/2.0" + '\r\n'
+#Creamos la cabecera
+CABECERA = "Expires: " + EXPIRES + '\r\n\r\n'
+
+LINE = LINE + CABECERA
 print "Enviando: " + LINE
 my_socket.send(LINE + '\r\n')
 data = my_socket.recv(1024)
