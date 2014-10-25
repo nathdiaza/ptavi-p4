3fa0182710998ca1abd1b31514d23a481843fca6
Irene <irene@ubuntu.(none)>
2014-10-25 19:18:39 +0200

    Ejercicio 7: Borramos cliente cuando expires = 0

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Clase (y programa principal) para un servidor de eco
+Clase (y programa principal) para un servidor de SIP
 en UDP simple
 """
 
@@ -20,21 +20,30 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         print self.client_address
-        self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             if not line:
                 break
-            print "El cliente nos manda " + line
+            print "El cliente nos manda: " + line
             EMAIL = line.split()[1][4:]
             IP = self.client_address[0]
-            print "Guardamos EMAIL: " + EMAIL + " y la IP: " + IP
-            dic_clients[EMAIL] = IP
+            self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
+            expires = int(line.split('\r\n')[1][8:])
+            print "Expires: " + str(expires)
+            
+            if expires == 0:
+                #Borramos al cliente del diccionario
+                del dic_clients[EMAIL]
+                print "Borramos a :" + EMAIL
+                self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
+            else:
+                print "Guardamos EMAIL: " + EMAIL + " y la IP: " + IP
+                dic_clients[EMAIL] = IP
 
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", PORT), SIPRegisterHandler)
-    print "Lanzando servidor UDP de eco..."
+    print "Lanzando servidor UDP de SIP..."
     serv.serve_forever()
