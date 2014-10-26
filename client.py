fa69affd903774e6a0915b3757fda9addf713e2f
Irene <irene@ubuntu.(none)>
2014-10-26 22:37:19 +0100

    Eliminados algunos mensajes de trazas

--- client.py
+++ client.py
@@ -17,11 +17,8 @@ if len(sys.argv) != 6:
 # Dirección IP del servidor.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-USUARIO = sys.argv[4]
+USER = sys.argv[4]
 EXPIRES = sys.argv[5]
-
-# Contenido que vamos a enviar
-#LINE = ' '.join(sys.argv[3:])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -29,7 +26,7 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 #Creamos la petición de registro
-LINE = "REGISTER" + " sip:" + USUARIO + " SIP/2.0" + '\r\n'
+LINE = "REGISTER" + " sip:" + USER + " SIP/2.0" + '\r\n'
 #Creamos la cabecera
 CABECERA = "Expires: " + EXPIRES + '\r\n\r\n'
 
