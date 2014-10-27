bdcbec9643e3a9c71071efc5c425add9cd09e646
Roger Urrutia Bayo <roger@zeta23.aulas.gsyc.es>
2014-10-27 15:29:51 +0100

    errores de variables

--- client.py
+++ client.py
@@ -9,18 +9,18 @@ import sys
 
 # Cliente UDP simple.
 
-LINE = ""
+line = ""
 try:
     # puerto y servidor.
-    SERVER = str(sys.argv[1])
-    PORT = int(sys.argv[2])
+    server = str(sys.argv[1])
+    port = int(sys.argv[2])
     # contenido a enviar.
     if sys.argv[3] == "register":
-        LINE = "REGISTER" + " sip:" + sys.argv[4] + " SIP/2.0\r\n"
-        LINE = LINE + "Expires: " + sys.argv[5] + "\r\n\r\n"
+        line = "REGISTER" + " sip:" + sys.argv[4] + " SIP/2.0\r\n"
+        line = line + "Expires: " + sys.argv[5] + "\r\n\r\n"
     else:
         for i in sys.argv[3:]:
-            LINE = LINE + str(i) + str(" ")
+            line = line + str(i) + str(" ")
 except ValueError:
     print "Usage: client.py ip puerto register sip_address expires_value"
     sys.exit()
@@ -28,10 +28,10 @@ except ValueError:
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((server, port))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+print "Enviando: " + line
+my_socket.send(line + '\r\n')
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
