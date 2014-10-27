1b3ad85b40a99ce5f2f5388a7a8fe267ca499a17
javier martinez molina <molina@iota25.aulas.gsyc.es>
2014-10-27 13:26:29 +0100

    actualizacion del fichero trabajando en el diccionario(hago todas las comprobaciones en el dic y actualizo el fichero pasando el dic), campo EXPIRES ya funciona( los usuarios con un EXPIRES inferior a la hora del usuario que ingresa son eliminados del fichero

--- server.py
+++ server.py
@@ -8,25 +8,15 @@ import time
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
 
     dic_registro = {}
-    fich = open("registered.txt", "a")
-    fich.write("User\t\t\t\t" + "IP\t\t\t" + "Expires\n")
-    fich.close()
     
-    def register2file(self, usuario, ip, tiempo, boolean):
-        if boolean:
-            fich = open("registered.txt", "a")
-            fich.write(usuario +"\t" + ip + "\t" + tiempo +"\n")
-            fich.close()
-        else:
-            fich = open("registered.txt", "r+")
-            lines = fich.readlines()
-            for line in lines:
-                palabras_line = line.split()
-                print palabras_line
-                if palabras_line[0] == usuario:
-                    lines.remove(line)
-            fich.close()
-            
+    def register2file(self):
+        fich = open("registered.txt", "w")
+        fich.write("User\t\t\t\t" + "IP\t\t\t" + "Expires\r\n")
+        for mail in self.dic_registro:
+            tiempo = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(self.dic_registro[mail][1]))
+            fich.write(mail +"\t" + self.dic_registro[mail][0] + "\t" + tiempo +"\r\n")
+        fich.close()
+       
     def handle(self):
     
         while 1:
@@ -35,17 +25,25 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             if line != "":
                 list_palabras = line.split()
                 if list_palabras[0] == "REGISTER":
+                    #Compruebo mi diccionario para localizar posibles users EXPIRES
+                    if self.dic_registro:
+                        tiempo_actual = time.time()
+                        for user in self.dic_registro.keys():
+                            if self.dic_registro[user][1] <= tiempo_actual:
+                                del self.dic_registro[user]
+                        self.register2file()
+                    #añado al user a la lista
+                    time_expired = time.time() + float(list_palabras[4])
                     recorte = list_palabras[1].split(":")
                     mail = recorte[1]
                     self.wfile.write("SIP/2.0 200 OK\r\n")
-                    tiempo = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
-                    self.register2file(mail, self.client_address[0], tiempo, 1)
-                    self.dic_registro[mail] = self.client_address[0]
-                    print self.dic_registro
+                    list_atributos = [self.client_address[0], time_expired]
+                    self.dic_registro[mail] = list_atributos
+                    self.register2file()
+                    #compruebo si el campos EXPIRES es 0
                     if int(list_palabras[4]) == 0:
-                        self.register2file(mail, self.client_address[0], tiempo, 0)
                         del self.dic_registro[mail]
-                        print self.dic_registro
+                        self.register2file()
                 print self.client_address
                 print line
             if not line:
