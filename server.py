6b9936a3910546050431aef67d5bb6c17fd379d1
irene lope rodriguez <ilope@iota17.aulas.gsyc.es>
2014-10-27 10:41:34 +0100

    Version final

--- server.py
+++ server.py
@@ -9,15 +9,14 @@ import SocketServer
 import sys
 import time
 
-
 PORT = int(sys.argv[1])
 dic_clients = {}
+
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     SIP server register class
     """
-
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         print self.client_address
@@ -25,66 +24,56 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             if not line:
-                break  
+                break
             print "El cliente nos manda: " + line
-            #Cogemos los datos del cliente 
+            #Cogemos los datos del cliente
             user = line.split()[1][4:]
             ip = self.client_address[0]
             metodo = line.split('\r\n')[0][:8]
             exp = int(line.split('\r\n')[1][8:])
-            
-            if metodo == 'REGISTER':  
-                hora_exp = time.time() + exp
-                expires = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(hora_exp))     
+            if metodo == 'REGISTER':
+                self.buscar_clientes()
                 if exp == 0:
                     #Borramos al cliente del diccionario
                     del dic_clients[user]
                     print "Borramos a :" + user
                 else:
+                    hora_exp = time.time() + exp
+                    formato = '%Y-%m-%d %H:%M:%S'
+                    expires = time.strftime(formato, time.gmtime(hora_exp))
                     print "Guardamos user: " + user + " y la IP: " + ip + '\n'
-                    dic_clients[user] = ip
-                    
-                self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n') 
-                self.register2file(user, ip, expires)
-                
-                print "DICCIONARIO CLIENTES:", dic_clients
-                print
+                    dic_clients[user] = [ip, expires]
+                self.register2file()
+                self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
+            else:
+                self.wfile.write("SIP/2.0 400 Bad Request" + '\r\n\r\n')
+            print "DICCIONARIO CLIENTES:", dic_clients
+            print
 
-            print "Buscamos clientes"
-            
-            self.buscar_clientes(dic_clients)
-
-    def register2file(self, user, ip, expires):
+    def register2file(self):
         """
         Registramos a los clientes en un fichero:
         """
-        fich = open('registered', 'r+')
-        linea = fich.readlines()
-        if linea == []:
-            fich.write('User' + '\t' + 'IP' + '\t' + 'Expires' + '\n')
-        fich.write(user + '\t' + ip + '\t' + str(expires) + '\n')
+        fich = open('registered', 'w')
+        fich.write('User' + '\t' + 'IP' + '\t' + 'Expires' + '\n')
+        for key in dic_clients.keys():
+            ip = dic_clients[key][0]
+            expire = str(dic_clients[key][1])
+            fich.write(key + '\t' + ip + '\t' + expire + '\n')
         fich.close()
 
-    def buscar_clientes(self, dic_clients):
+    def buscar_clientes(self):
         """
-        Buscamos si ha caducado el expire de los clientes y en ese caso se borran
+        Buscamos si han caducado los clientes y borrarlos
         """
-        fich = open('registered', 'r')
-        linea = fich.readlines()
-        for cliente in linea:
-            if len(cliente) != 16: #No es la primera linea
-                expires = cliente.split(' ')[1]
-                hora = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
-                hora = hora.split(' ')[1]
-                if expires == hora:
-                    user = cliente.split(' ')[0]
-                    print user
-                    del dic_clients[user]
-                    print "Borramos a :" + user
-                    self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
-        fich.close()
-    
-    
+        for key in dic_clients.keys():
+            expires = dic_clients[key][1]
+            hora = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+            if expires <= hora:
+                del dic_clients[key]
+                print "Borramos a :" + key
+
+
 if __name__ == "__main__":
     # Creamos servidor register y escuchamos
     serv = SocketServer.UDPServer(("", PORT), SIPRegisterHandler)
