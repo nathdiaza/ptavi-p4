231db32b0e0b3cae67ca17eb6bce7aa9a19a0504
Juan Carlos Diaz Bautista <jcdb@iota34.aulas.gsyc.es>
2014-10-27 10:37:53 +0100

    Guarda en un fichero y gestiona la caducidad

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ en UDP simple
 
 import SocketServer
 import sys
+import time
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
@@ -14,6 +15,13 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     List_Client = {}
     def handle(self):
+    
+    	for x in self.List_Client.keys():
+    	
+		if self.List_Client[x][1]< time.time():
+			del self.List_Client[x]
+			print self.List_Client
+		
         # Escribe dirección y puerto del cliente (de tupla client_address)
    
         while 1:
@@ -22,15 +30,29 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             print line
             line = line.split()
             if "REGISTER" in line:
-     
-            	self.List_Client[line[2]] = self.client_address
+     		if line[5] == "0":
+     			if line[2] in self.List_Client:
+     				del self.List_Client[line[2]]
+     		else :
+            		self.List_Client[line[2]] = (self.client_address[0],time.time()+float(line[5]))
             	self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-            	print self.List_Client
+            	self.register2file()	
+            	
             if not line:
                 break
+                
+                
+    def register2file(self):
+    
+    	fich = open("registered.txt", 'w')
+    	line = ("Use"+ "\t" + "IP" + "\t" + "Expires"+ "\r\n")
+    	for x in self.List_Client.keys():
+    		h_Expires = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(self.List_Client[x][1]))
+    		line += x + "\t" + self.List_Client[x][0] + "\t" + h_Expires + "\r\n"
+	fich.write(line)    
+    	fich.close()
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("",int(sys.argv[1]) ), SIPRegisterHandler)
-    print "Lanzando servidor UDP de eco..."
+    serv = SocketServer.UDPServer(("",int(sys.argv[1])), SIPRegisterHandler)
     serv.serve_forever()
