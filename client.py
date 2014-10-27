3f1569eb2eca096be17db4a77793fdfe3dd69938
Juan Carlos Diaz Bautista <jcdb@iota34.aulas.gsyc.es>
2014-10-27 10:38:51 +0100

    client ip borrado tramas profesor

--- client.py
+++ client.py
@@ -9,26 +9,27 @@ import sys
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
 
-# Contenido que vamos a enviar
-LINE =' '.join(sys.argv[3:]) 
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    METHOD = sys.argv[3]
+    SIP_ADDRES = sys.argv[4]
+    EXPIRES_VALUE = sys.argv[5]
 
 
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+    if METHOD == "register":
+	    my_socket.send("REGISTER sip: " + SIP_ADDRES + " SIP/2.0" + '\r\n'+ "Expires: " + EXPIRES_VALUE + '\r\n\r\n')
+	    data = my_socket.recv(1024)
+	    print  data   
+    my_socket.close()
 
-print "Enviando: "
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
-
-print 'Recibido -- ', data
-print "Terminando socket..."
-
+	    
+except (IndexError, ValueError):
+   print "Usage: client.py ip puerto register sip_address expires_value"
 # Cerramos todo
-my_socket.close()
-print "Fin."
+    
