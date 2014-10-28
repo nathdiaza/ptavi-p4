f32f8cf4a81c73739645b94bbd7c4a4cc6cff471
nathdiaza <nath.diaza@gmail.com>
2014-10-28 15:56:38 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Clase (y programa principal) para un servidor de eco
+Clase (y programa principal) para un servidor de SIP
 en UDP simple
 """
 
@@ -17,43 +17,44 @@ except ValueError:
     print "Error: los parametros pasados son erroneos"
     sys.exit()
 
-d = {}
+dicc = {}
 
 
 def register2file():
     deletekey = ""
     fichero1 = open("registered.txt", "w")
     fichero1.write("User\tIP\tExpires\n")
-    for key in d:
-        fichero1.write(key + "\t" + d[key][0] + "\t" + d[key][1] + "\n")
+    for key in dicc:
+        fichero1.write(key + "\t" + dicc[key][0] + "\t" + dicc[key][1] + "\n")
     fichero1.close
     fichero1 = open("registered.txt", "r")
     linea = fichero1.readline()
-    for key in d:
+    for key in dicc:
         linea = fichero1.readline()
         hora = linea.split("\t")[2].split("\n")[0]
         horafrmt = time.mktime(time.strptime(hora, "%y/%m/%d %H:%M:%S")) + 3600
         if horafrmt < time.time():
             deletekey = key
     if not deletekey == "":
-        del d[deletekey]
+        del dicc[deletekey]
     fichero1.close
     fichero2 = open("registered.txt", "w")
     fichero2.write("User\tIP\tExpires\n")
-    for key in d:
-        fichero2.write(key + "\t" + d[key][0] + "\t" + d[key][1] + "\n")
+    for key in dicc:
+        fichero2.write(key + "\t" + dicc[key][0] + "\t" + dicc[key][1] + "\n")
     fichero2.close
 
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    SIP server class
     """
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
+            print "El cliente nos manda: " + line
+            # Escribe dirección y puerto del cliente (de tupla client_address)
             inf_client = self.client_address
             ip = str(inf_client[0])
             puerto = str(inf_client[1])
@@ -64,20 +65,20 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                     expires = line.split(":")[2].split(" ")[1]
                     hora = time.gmtime(time.time() + int(expires))
                     horaformato = time.strftime("%y/%m/%d %H:%M:%S", hora)
-                    d[direccion] = (ip, horaformato)
+                    dicc[direccion] = (ip, horaformato)
+                    print "entra: " +  str(direccion) + "\r"
                     if int(expires) == 0:
-                        del d[direccion]
+                        del dicc[direccion]
+                        print "borro a: " +  str(direccion) + "\r"
                     register2file()
 
                 else:
-                    self.wfile.write("Hemos recibido tu peticion")
-                    print "IP: " + ip + " Port: " + puerto
-                    print "El cliente nos manda " + line + "\n"
+                    self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
             else:
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", PORT), SIPRegisterHandler)
-    print "Lanzando servidor UDP de eco..."
+    print "Lanzando servidor UDP de SIP..."
     serv.serve_forever()
