f93747a2f69f1ac7614d6a44eef88bbbc9d82557
nathdiaza <nath.diaza@gmail.com>
2014-10-28 22:12:28 +0100

    Update server.py

--- server.py
+++ server.py
@@ -21,13 +21,22 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
         y en sucesivas líneas los valores de cada user registrado.
         """
         fich = open("registered.txt", "w")
-        fich.write("User\t" + "IP\t" + "Expires\r\n")
+        fich.write("User\tIP\tExpires\r\n")
         for mail in self.dic_reg:
             segundos = self.dic_reg[mail][1]
             t = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(segundos))
             fich.write(mail + "\t" + self.dic_reg[mail][0] + "\t" + t + "\r\n")
         fich.close()
-
+    
+    def ver_si_expira(self):
+        if self.dic_reg:
+            tiempo_actual = time.time()
+            for user in self.dic_reg.keys():
+                if self.dic_reg[user][1] <= tiempo_actual:
+                    del self.dic_reg[user]
+                    print "Expira del dicc: " + str(user) + "\r"
+            self.register2file()
+    
     def handle(self):
         """
         Se comprueba que el tipo de mensaje es un REGISTER,
@@ -38,27 +47,28 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             if line != "":
+                print "El cliente nos manda: " + line
                 list_palabras = line.split()
                 if list_palabras[0] == "REGISTER":
                     #Compruebo mi dic para localizar posibles users EXPIRES
-                    if self.dic_reg:
-                        tiempo_actual = time.time()
-                        for user in self.dic_reg.keys():
-                            if self.dic_reg[user][1] <= tiempo_actual:
-                                del self.dic_reg[user]
-                        self.register2file()
+                    self.ver_si_expira()
                     #añado al user a la lista
                     time_expired = time.time() + float(list_palabras[4])
                     recorte = list_palabras[1].split(":")
                     mail = recorte[1]
                     self.wfile.write("SIP/2.0 200 OK\r\n")
-                    list_atributos = [self.client_address[0], time_expired]
-                    self.dic_reg[mail] = list_atributos
-                    self.register2file()
                     #compruebo si el campos EXPIRES es 0
                     if int(list_palabras[4]) == 0:
-                        del self.dic_reg[mail]
-                        self.register2file()
+                        # borro a ese mail, si estaba ya en el diccionario
+                        if mail in self.dic_reg:
+                            del self.dic_reg[mail]
+                            print "Borro del dicc a: " + str(mail) + "\r"
+                    else: 
+                        # introduzco en el dic sólo si su expire no es 0
+                        list_atributos = [self.client_address[0], time_expired]
+                        self.dic_reg[mail] = list_atributos
+                        print "Entra en el dicc: " + str(mail) + "\r"
+                    self.register2file()
                     print self.client_address
                     print line
                 else:
@@ -69,5 +79,5 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
 if __name__ == "__main__":
 
     serv = SocketServer.UDPServer(("", int(sys.argv[1])), SIPRegisterHandler)
-    print "Lanzando servidor UDP de eco...\n"
+    print "Lanzando servidor UDP de SIP...\n"
     serv.serve_forever()
