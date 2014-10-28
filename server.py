0826ed9c62c289d57dce8cf0c0a6901191aa804c
nathdiaza <nath.diaza@gmail.com>
2014-10-28 15:27:17 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Clase (y programa principal) para un servidor de eco
+Clase (y programa principal) para un servidor de SIP
 en UDP simple
 """
 
@@ -11,33 +11,37 @@ import time
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    SIP server class
     """
     List_Client = {}
     def handle(self):
     
-    	for x in self.List_Client.keys():
+    	for client in List_Client.keys():
     	
-		if self.List_Client[x][1]< time.time():
-			del self.List_Client[x]
-			print self.List_Client
+		if List_Client[client][1]< time.time():
+			del List_Client[client]
+			print "Expira: " + str(client) + "\r"
+			# Imprimir sólo si no está vacío
+			if List_Client:
+				print List_Client
 		
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-   
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print line
+            print "El cliente manda: " + line
             line = line.split()
             if "REGISTER" in line:
      		if line[5] == "0":
-     			if line[2] in self.List_Client:
-     				del self.List_Client[line[2]]
+     			if line[2] in List_Client:
+     				del List_Client[line[2]]
+     				print "Borramos a: " + str(line[2]) + "\r"
      		else :
-            		self.List_Client[line[2]] = (self.client_address[0],time.time()+float(line[5]))
+            		List_Client[line[2]] = (self.client_address[0],time.time()+float(line[5]))
+            		print "Entra: " + str(line[2]) + "\r"
             	self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
             	self.register2file()	
-            	
+            else:
+            	self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n"
             if not line:
                 break
                 
@@ -46,13 +50,14 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     
     	fich = open("registered.txt", 'w')
     	line = ("Use"+ "\t" + "IP" + "\t" + "Expires"+ "\r\n")
-    	for x in self.List_Client.keys():
-    		h_Expires = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(self.List_Client[x][1]))
-    		line += x + "\t" + self.List_Client[x][0] + "\t" + h_Expires + "\r\n"
+    	for client in List_Client.keys():
+    		h_Expires = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(List_Client[client][1]))
+    		line += x + "\t" + List_Client[client][0] + "\t" + h_Expires + "\r\n"
 	fich.write(line)    
     	fich.close()
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
+    # Creamos servidor de SIP y escuchamos
     serv = SocketServer.UDPServer(("",int(sys.argv[1])), SIPRegisterHandler)
+    print "\nLanzando servidor UDP de SIP... \r\n"
     serv.serve_forever()
